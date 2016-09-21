0a02af598ae971175663b6b27ccdb0aa47d3ae18
Aron Wouters <lionar@mail.com>
2016-09-21 12:20:21 +0200

    Created array except helper.

---
+++ helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Return a copy of a given array without the specified keys.
+ * 
+ * @param  array  $keys  		The keys to remove from the array.
+ * @param  array  $array 		The array to remove the keys from
+ * @return array        		The array without specified keys.
+ */
+function array_except ( array $keys, array $array ) : array
+{
+	foreach ( $keys as $key )
+		if ( isset ( $array [ $key ] ) )
+			unset ( $array [ $key ] );
+	return $array;
+}

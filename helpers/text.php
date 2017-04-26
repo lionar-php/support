e45ca5c92e2791baba24715b8d00e7f4beafee6d
Aron Wouters <lionar@mail.com>
2017-04-27 00:47:21 +0200

    Added grammar text functions.

--- helpers/text.php
+++ helpers/text.php
@@ -5,6 +5,11 @@ namespace Text;
 function capitalize ( string $text )
 {
 	return ucfirst ( strtolower ( $text ) );
+}
+
+function capitalized ( string $text )
+{
+	return capitalize ( $text );
 } 
 
 function uppercase ( string $text )
@@ -12,7 +17,17 @@ function uppercase ( string $text )
 	return strtoupper ( $text );
 }
 
+function uppercased ( string $text )
+{
+	return uppercase ( $text );
+}
+
 function lowercase ( string $text )
 {
 	return strtolower ( $text );
+}
+
+function lowercased ( string $text )
+{
+	return lowercase ( $text );
 }
